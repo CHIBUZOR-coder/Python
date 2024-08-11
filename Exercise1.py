919c2f95ac1790a3eee06708639638184454c532
Chibuzor <chibuzormekalam@gmail.com>
2024-08-11 12:36:19 +0100

    finished bmi exercise

--- Exercise1.py
+++ Exercise1.py
@@ -1,13 +1,16 @@
+# Exercise1
+# =================================
 mass = input("Enter weight in kg")
 height = input("Enter height in meter")
 
-# bmi = int(mass)/float(height)
+bmi = int(mass) / float(height) ** 2
+print("bmi:" + str(int(bmi)) + "kg")
+
+print(type(bmi))
+# length = len(bmi);
+# sum = int(bmi[0]) + int(bmi[1])
 # print(bmi)
-num = int(mass) + float(height)
-bmi = str(mass) + str(height)
-length = len(bmi);
-sum = int(bmi[0]) + int(bmi[1])
-print(bmi)
-print(num)
-print(sum)
+# print(num)
+# print(sum)
 # print
+# =====================================
